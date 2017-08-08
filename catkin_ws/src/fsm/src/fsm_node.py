284399e34e8e76c2b923b76b130255bb59880759
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-08-08 23:25:47 +0800

    set up LED

--- catkin_ws/src/fsm/src/fsm_node.py
+++ catkin_ws/src/fsm/src/fsm_node.py
@@ -8,7 +8,8 @@ import Adafruit_PCA9685
 class FSMNode(object):
     def __init__(self):
         self.node_name = rospy.get_name()
-
+        self.pwm = Adafruit_PCA9685.PCA9685()
+        self.pwm.set_pwm_freq(60)
         # Build transition dictionray
         self.states_dict = rospy.get_param("~states",{})        
         # Validate state and global transitions
