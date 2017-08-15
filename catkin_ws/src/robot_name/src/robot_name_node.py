82ec0b5b7b15544e9f678b67a2ce2bcbc424ddea
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-08-16 02:53:26 +0800

    get robot name

--- catkin_ws/src/robot_name/src/robot_name_node.py
+++ catkin_ws/src/robot_name/src/robot_name_node.py
@@ -11,9 +11,10 @@ class carName(object):
         # Setup parameters
         self.name = rospy.get_param('~name')
         # Publicaiton
-        self.pub_name = rospy.Publisher("~robot_name", RobotName, queue_size=1)
+        self.pub_name = rospy.Publisher("robot_name", RobotName, queue_size=1)
 
         # safe shutdown
+        self.sub_name = rospy.Subscriber("robot_name", RobotName, self.subname, queue_size=1)
 
         self.publishName()
 
@@ -22,6 +23,9 @@ class carName(object):
         name_msg = RobotName()
         name_msg.robot_name = self.name
         self.pub_name.publish(name_msg)
+
+    def subname(self, msg):
+        rospy.loginfo(msg)
 
 if __name__ == "__main__":
     rospy.init_node("robot_name",anonymous=False)
