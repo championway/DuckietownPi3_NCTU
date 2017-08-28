f24e4f0c2b83004699866f71e999701683d2ae34
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-08-28 13:48:46 +0800

    solve line detector

--- catkin_ws/src/line_detector/include/line_detector/line_detector1.py
+++ catkin_ws/src/line_detector/include/line_detector/line_detector1.py
@@ -131,7 +131,7 @@ class LineDetectorHSV(Configurable, LineDetectorInterface):
         return edges
 
     def _HoughLine(self, edge):
-        lines = cv2.HoughLines(edge, 1, np.pi/180, self.hough_threshold, 80, self.hough_min_line_length, self.hough_max_line_gap)
+        lines = cv2.HoughLines(edge, 1, np.pi/180, self.hough_threshold, 1, self.hough_min_line_length, self.hough_max_line_gap)
         if lines is not None:
             lines = np.array(lines[0])
         else:
