a5e8a98171256c516302118780d4fa02ff9fcc53
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-08-28 23:33:32 +0800

    multi-robot broadcast

--- catkin_ws/src/master/src/master_name_node.py
+++ catkin_ws/src/master/src/master_name_node.py
@@ -84,7 +84,6 @@ class carName(object):
 
     def AllRobot(self, pub, msg):
         for i in self.robotlist.keys():
-            print i
             self.robot = i
             self.pub_car_cmd = rospy.Publisher("/"+self.robot+"/joy_mapper_node/car_cmd", Twist2DStamped, queue_size=1)
             self.pub_pressA = rospy.Publisher("/"+self.robot+"/joy_mapper_node/press_A",BoolStamped,queue_size=1)
