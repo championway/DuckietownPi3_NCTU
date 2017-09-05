494ade7a1145fd7f4adf5703771411d0ca56d6c8
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-09-05 23:18:39 +0800

    Update socket_node.py

--- catkin_ws/src/android_app/src/socket_node.py
+++ catkin_ws/src/android_app/src/socket_node.py
@@ -21,7 +21,7 @@ if "__main__" == __name__:
     except:
         print("init socket err!")
     while True:
-        if con=False
+        if con==False
             print("listen for client...")
             conn, addr = sock.accept()
             print("get client")
