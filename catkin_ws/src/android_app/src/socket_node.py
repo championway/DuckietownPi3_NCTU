31bf9d75f7e013ca1ce15c6c33c37ddeeb3a052e
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-09-05 17:50:00 +0800

    Update socket_node.py

--- catkin_ws/src/android_app/src/socket_node.py
+++ catkin_ws/src/android_app/src/socket_node.py
@@ -24,6 +24,7 @@ if "__main__" == __name__:
         conn, addr = sock.accept()
         print("get client")
         print(addr)
+        conn.send("welcome client!'")
         #conn.settimeout(5)
         szBuf = conn.recv(1024)
         print("recv:" + szBuf)
