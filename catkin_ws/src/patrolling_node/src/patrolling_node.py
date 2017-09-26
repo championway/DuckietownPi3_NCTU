5b2653912a5df8299defc2e1198530f718758642
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-09-27 00:36:01 +0800

    2017/09/26

--- catkin_ws/src/patrolling_node/src/patrolling_node.py
+++ catkin_ws/src/patrolling_node/src/patrolling_node.py
@@ -2,8 +2,8 @@
 import rospkg
 import rospy
 import yaml
-from std_msgs import Float64
-from duckietown_msgs.msg import AprilTagsWithInfos, TagInfo, AprilTagDetectionArray, BoolStamped, FSMState
+from std_msgs import Int8
+from duckietown_msgs.msg import PatrolBot, BoolStamped
 import numpy as np
 import tf.transformations as tr
 from geometry_msgs.msg import PoseStamped
@@ -11,122 +11,187 @@ from geometry_msgs.msg import PoseStamped
 class PatrollingNode(object):
 
     def __init__(self):
-    	self.start = False
-
-    	self.tag1_left_cost = Float64()
-    	self.tag1_right_cost = Float64()
-    	self.tag2_left_cost = Float64()
-    	self.tag2_right_cost = Float64()
-    	self.tag3_left_cost = Float64()
-    	self.tag3_right_cost = Float64()
-    	self.tag4_left_cost = Float64()
-    	self.tag4_right_cost = Float64()
-
-    	self.tag1_left_start = self.timer_start
-		self.tag1_right_start = self.timer_start
-		self.tag2_left_start = self.timer_start
-		self.tag2_right_start = self.timer_start
-		self.tag3_left_start = self.timer_start
-		self.tag3_right_start = self.timer_start
-		self.tag4_left_start = self.timer_start
-		self.tag4_right_start = self.timer_start
-
+        #initial
+        self.start = False
         self.node_name = "patrolling_node"
 
-        self.sub_left = rospy.Subscriber("~tag1_left",BoolStamped, self.tag1_left)
-        self.sub_right = rospy.Subscriber("~tag1_right",BoolStamped, self.tag1_right)
-        self.sub_left = rospy.Subscriber("~tag2_left",BoolStamped, self.tag2_left)
-        self.sub_right = rospy.Subscriber("~tag2_right",BoolStamped, self.tag2_right)
-        self.sub_left = rospy.Subscriber("~tag3_left",BoolStamped, self.tag3_left)
-        self.sub_right = rospy.Subscriber("~tag3_right",BoolStamped, self.tag3_right)
-        self.sub_left = rospy.Subscriber("~tag4_left",BoolStamped, self.tag4_left)
-        self.sub_right = rospy.Subscriber("~tag4_right",BoolStamped, self.tag4_right)
+        #cost of each node
+        self.left1_cost = 0
+        self.right1_cost = 0
+        self.left2_cost = 0
+        self.right2_cost = 0
+        self.left3_cost = 0
+        self.right3_cost = 0
+        self.left4_cost = 0
+        self.right4_cost = 0
 
-        self.pub_tag1_turn_left = rospy.Publisher("~tag1_left_cost", Float64, queue_size=1)
-        self.pub_tag1_turn_right = rospy.Publisher("~tag1_right_cost", Float64, queue_size=1)
-        self.pub_tag2_turn_left = rospy.Publisher("~tag2_left_cost", Float64, queue_size=1)
-        self.pub_tag2_turn_right = rospy.Publisher("~tag2_right_cost", Float64, queue_size=1)
-        self.pub_tag3_turn_left = rospy.Publisher("~tag3_left_cost", Float64, queue_size=1)
-        self.pub_tag3_turn_right = rospy.Publisher("~tag3_right_cost", Float64, queue_size=1)
-        self.pub_tag4_turn_left = rospy.Publisher("~tag4_left_cost", Float64, queue_size=1)
-        self.pub_tag4_turn_right = rospy.Publisher("~tag4_right_cost", Float64, queue_size=1)
+        #to see each node are target or not
+        self.left1_target = False
+        self.right1_target = False
+        self.left2_target = False
+        self.right2_target = False
+        self.left3_target = False
+        self.right3_target = False
+        self.left4_target = False
+        self.right4_target = False
 
+        #iniital starting time of each node
+        self.left1_start = self.timer_start
+        self.right1_start = self.timer_start
+        self.left2_start = self.timer_start
+        self.right2_start = self.timer_start
+        self.left3_start = self.timer_start
+        self.right3_start = self.timer_start
+        self.left4_start = self.timer_start
+        self.right4_start = self.timer_start
+
+        #======Subscriber======
+        self.sub_robot_info = rospy.Subscriber("~sub_robot", PatrolBot, self.sub_robot)
+
+        #======Publisher======
+        self.pub_command = rospy.Publisher("~command", Int8, queue_size=1)
+
+        #======start to count the time======
         self.start_time()
 
-    def put_cost(self):
-        self.pub_tag1_turn_left.publish(self.tag1_left_cost)
-        self.pub_tag1_turn_right.publish(self.tag1_right_cost)
-        self.pub_tag2_turn_left.publish(self.tag2_left_cost)
-        self.pub_tag2_turn_right.publish(self.tag2_right_cost)
-        self.pub_tag3_turn_left.publish(self.tag3_left_cost)
-        self.pub_tag3_turn_right.publish(self.tag3_right_cost)
-        self.pub_tag4_turn_left.publish(self.tag4_left_cost)
-        self.pub_tag4_turn_right.publish(self.tag4_right_cost)
+    #suppose msg.name--> robotrname msg.tag--> current tag ex:left1, right3
+    def sub_robot(self, msg):
+        self.count_cost()
+        cmd = Int8()
+        cmd.data = 0 # 1=forward 2=turnaround
+        self.pub_command = rospy.Publisher("/"+msg.name+"/command", BoolStamped, queue_size=1)
+        tar = "self." + msg.name + "_cost"
+        vars()[tar] = False
+        self.count_target()
 
+        if msg.tag == "left1":
+            if self.right1_cost > self.right2_cost:
+                self.right1_start = time.time()
+                self.left4_target = True
+                cmd.data = 1
+            else:
+                self.right2_target = True
+                cmd.data = 2
+            self.left1_start = time.time()
+
+        elif msg.tag == "right1":
+            if self.left1_cost > self.left4_cost:
+                self.right2_target = True
+                self.left1_start = time.time()
+                cmd.data = 1
+            else:
+                self.left4_target = True
+                cmd.data = 2
+            self.right1_start = time.time()
+
+        elif msg.tag == "left2":
+            if self.right2_cost > self.right3_cost:
+                self.right2_start = time.time()
+                self.left1_target = True
+                cmd.data = 1
+            else:
+                self.right3_target = True
+                cmd.data = 2
+            self.left1_start = time.time()
+
+        elif msg.tag == "right2":
+            if self.left2_cost > self.left1_cost:
+                self.right3_target = True
+                self.left2_start = time.time()
+                cmd.data = 1
+            else:
+                self.left1_target = True
+                cmd.data = 2
+            self.right2_start = time.time()
+
+        elif msg.tag == "left3":
+            if self.right3_cost > self.right4_cost:
+                self.right3_start = time.time()
+                self.left2_target = True
+                cmd.data = 1
+            else:
+                self.right4_target = True
+                cmd.data = 2
+            self.left3_start = time.time()
+
+        elif msg.tag == "right3":
+            if self.left3_cost > self.left2_cost:
+                self.right4_target = True
+                self.left3_start = time.time()
+                cmd.data = 1
+            else:
+                self.left2_target = True
+                cmd.data = 2
+            self.right3_start = time.time()
+
+        elif msg.tag == "left4":
+            if self.right4_cost > self.right1_cost:
+                self.right4_start = time.time()
+                self.left3_target = True
+                cmd.data = 1
+            else:
+                self.right1_target = True
+                cmd.data = 2
+            self.left4_start = time.time()
+
+        elif msg.tag == "right4":
+            if self.left4_cost > self.left3_cost:
+                self.right1_target = True
+                self.left4_start = time.time()
+                cmd.data = 1
+            else:
+                self.left3_target = True
+                cmd.data = 2
+            self.right4_start = time.time()
+
+        self.pub_command.publish(cmd)
+
+    #count the cost of each node (idleness)
+    def count_cost(self):
+        self.left1_cost = count_time(self.left1_start)
+        self.right1_cost = count_time(self.right1_start)
+        self.left2_cost = count_time(self.left2_start)
+        self.right2_cost = count_time(self.right2_start)
+        self.left3_cost = count_time(self.left3_start)
+        self.right3_cost = count_time(self.right3_start)
+        self.left4_cost = count_time(self.left4_start)
+        self.right4_cost = count_time(self.right4_start)
+
+    #initial time of all the nodes
     def start_time(self):
-		if not self.start: # if timer not start yet
-			self.timer_start = time.time() # record start time
-			self.tag1_left_start = self.timer_start
-			self.tag1_right_start = self.timer_start
-			self.tag2_left_start = self.timer_start
-			self.tag2_right_start = self.timer_start
-			self.tag3_left_start = self.timer_start
-			self.tag3_right_start = self.timer_start
-			self.tag4_left_start = self.timer_start
-			self.tag4_right_start = self.timer_start
+        if not self.start: # if timer not start yet
+            self.timer_start = time.time() # record start time
+            self.left1_start = self.timer_start
+            self.right1_start = self.timer_start
+            self.left2_start = self.timer_start
+            self.right2_start = self.timer_start
+            self.left3_start = self.timer_start
+            self.right3_start = self.timer_start
+            self.left4_start = self.timer_start
+            self.right4_start = self.timer_start
             self.start = True # change timer state to start
 
+    #return current time - starting time
     def count_time(self, t):
-		return time.time()-t
+        return time.time()-t
 
-    def tag1_left(self, msg):
-    	if msg.data == True:
-    		self.tag1_left_cost.data = count_time(self.tag1_left_start)
-            self.pub_cost()
-    		self.tag1_left_start = time.time()
-
-    def tag1_right(self, msg):
-    	if msg.data == True:
-    		self.tag1_right_cost.data = count_time(self.tag1_right_start)
-            self.pub_cost()
-    		self.tag1_right_start = time.time()
-
-    def tag2_left(self, msg):
-    	if msg.data == True:
-    		self.tag2_left_cost.data = count_time(self.tag2_left_start)
-            self.pub_cost()
-    		self.tag2_left_start = time.time()
-
-    def tag2_right(self, msg):
-    	if msg.data == True:
-    		self.tag2_right_cost.data = count_time(self.tag2_left_start)
-            self.pub_cost()
-    		self.tag2_right_start = time.time()
-
-    def tag3_left(self, msg):
-    	if msg.data == True:
-    		self.tag3_left_cost.data = count_time(self.tag4_left_start)
-            self.pub_cost()
-    		self.tag3_left_start = time.time()
-
-
-    def tag3_right(self, msg):
-    	if msg.data == True:
-    		self.tag3_right_cost.data = count_time(self.tag3_right_start)
-            self.pub_cost()
-    		self.tag3_right_start = time.time()
-
-    def tag4_left(self, msg):
-    	if msg.data == True:
-    		self.tag4_left_cost.data = count_time(self.tag4_left_start)
-            self.pub_cost()
-    		self.tag4_left_start = time.time()
-
-    def tag4_right(self, msg):
-    	if msg.data == True:
-    		self.tag4_right_cost.data = count_time(self.tag4_right_start)
-            self.pub_cost()
-    		self.tag4_right_start = time.time()
+    def count_target(self):
+        if self.left1_target:
+            self.left1_cost = 0
+        if self.right1_target:
+            self.right1_cost = 0
+        if self.left2_target:
+            self.left2_cost = 0
+        if self.right2_target:
+            self.right2_cost = 0
+        if self.left3_target:
+            self.left3_cost = 0
+        if self.right3_target:
+            self.right3_cost = 0
+        if self.left4_target:
+            self.left4_cost = 0
+        if self.right4_target:
+            self.right4_cost = 0
 
 if __name__ == '__main__': 
     rospy.init_node('PatrollingNode',anonymous=False)
--- catkin_ws/src/patrolling_node/src/test.py
+++ catkin_ws/src/patrolling_node/src/test.py
@@ -2,7 +2,7 @@
 import rospkg
 import rospy
 import yaml
-from std_msgs import Int8
+from std_msgs import Float64
 from duckietown_msgs.msg import AprilTagsWithInfos, TagInfo, AprilTagDetectionArray, BoolStamped, FSMState
 import numpy as np
 import tf.transformations as tr
@@ -11,42 +11,28 @@ from geometry_msgs.msg import PoseStamped
 class PatrollingNode(object):
 
     def __init__(self):
-    	#initial
-    	self.start = False
+        self.start = False
+
+        self.tag1_left_cost = Float64()
+        self.tag1_right_cost = Float64()
+        self.tag2_left_cost = Float64()
+        self.tag2_right_cost = Float64()
+        self.tag3_left_cost = Float64()
+        self.tag3_right_cost = Float64()
+        self.tag4_left_cost = Float64()
+        self.tag4_right_cost = Float64()
+
+        self.tag1_left_start = self.timer_start
+        self.tag1_right_start = self.timer_start
+        self.tag2_left_start = self.timer_start
+        self.tag2_right_start = self.timer_start
+        self.tag3_left_start = self.timer_start
+        self.tag3_right_start = self.timer_start
+        self.tag4_left_start = self.timer_start
+        self.tag4_right_start = self.timer_start
+
         self.node_name = "patrolling_node"
 
-    	#cost of each node
-    	self.tag1_left_cost = 0
-    	self.tag1_right_cost = 0
-    	self.tag2_left_cost = 0
-    	self.tag2_right_cost = 0
-    	self.tag3_left_cost = 0
-    	self.tag3_right_cost = 0
-    	self.tag4_left_cost = 0
-    	self.tag4_right_cost = 0
-
-    	#to see each node are target or not
-    	self.left1_target = False
-    	self.right1_target = False
-    	self.left2_target = False
-    	self.right2_target = False
-    	self.left3_target = False
-    	self.right3_target = False
-    	self.left4_target = False
-    	self.right4_target = False
-
-    	#iniital starting time of each node
-    	self.tag1_left_start = self.timer_start
-		self.tag1_right_start = self.timer_start
-		self.tag2_left_start = self.timer_start
-		self.tag2_right_start = self.timer_start
-		self.tag3_left_start = self.timer_start
-		self.tag3_right_start = self.timer_start
-		self.tag4_left_start = self.timer_start
-		self.tag4_right_start = self.timer_start
-
-		#======Subscriber======
-        self.sub_robot_info = rospy.Subscriber("~sub_robot",BoolStamped, self.tag1_left)
         self.sub_left = rospy.Subscriber("~tag1_left",BoolStamped, self.tag1_left)
         self.sub_right = rospy.Subscriber("~tag1_right",BoolStamped, self.tag1_right)
         self.sub_left = rospy.Subscriber("~tag2_left",BoolStamped, self.tag2_left)
@@ -56,77 +42,91 @@ class PatrollingNode(object):
         self.sub_left = rospy.Subscriber("~tag4_left",BoolStamped, self.tag4_left)
         self.sub_right = rospy.Subscriber("~tag4_right",BoolStamped, self.tag4_right)
 
-        #======Publisher======
-        self.pub_command = rospy.Publisher("~command", Int8, queue_size=1)
+        self.pub_tag1_turn_left = rospy.Publisher("~tag1_left_cost", Float64, queue_size=1)
+        self.pub_tag1_turn_right = rospy.Publisher("~tag1_right_cost", Float64, queue_size=1)
+        self.pub_tag2_turn_left = rospy.Publisher("~tag2_left_cost", Float64, queue_size=1)
+        self.pub_tag2_turn_right = rospy.Publisher("~tag2_right_cost", Float64, queue_size=1)
+        self.pub_tag3_turn_left = rospy.Publisher("~tag3_left_cost", Float64, queue_size=1)
+        self.pub_tag3_turn_right = rospy.Publisher("~tag3_right_cost", Float64, queue_size=1)
+        self.pub_tag4_turn_left = rospy.Publisher("~tag4_left_cost", Float64, queue_size=1)
+        self.pub_tag4_turn_right = rospy.Publisher("~tag4_right_cost", Float64, queue_size=1)
 
-        #======start to count the time======
         self.start_time()
 
-    #suppose msg.car--> carname msg.tag--> current tag
-    def sub_robot(self, msg):
-    	self.count_cost()
-    	cmd ==
-    	if msg.tag == "left1":
-    		self.tag1_left_start = time.time()
+    def put_cost(self):
+        self.pub_tag1_turn_left.publish(self.tag1_left_cost)
+        self.pub_tag1_turn_right.publish(self.tag1_right_cost)
+        self.pub_tag2_turn_left.publish(self.tag2_left_cost)
+        self.pub_tag2_turn_right.publish(self.tag2_right_cost)
+        self.pub_tag3_turn_left.publish(self.tag3_left_cost)
+        self.pub_tag3_turn_right.publish(self.tag3_right_cost)
+        self.pub_tag4_turn_left.publish(self.tag4_left_cost)
+        self.pub_tag4_turn_right.publish(self.tag4_right_cost)
 
-    	if msg.tag == "right1":
-            if self.tag2_right_cost > self.tag4_left_cost:
-                self.right2_target = True
-                self.left1_target = True
-                self.pub_command = rospy.Publisher("/"+msg.car+"/command", BoolStamped, queue_size=1)
-                self.pub_command.publish(self.tag4_right_cost)
-            else:
-                self.left4_target = True
-    		self.tag1_right_start = time.time()
-
-    	if msg.tag == "left2":
-    		self.tag2_left_start = time.time()
-
-    	if msg.tag == "right2":
-    		self.tag2_right_start = time.time()
-
-    	if msg.tag == "left3":
-    		self.tag3_left_start = time.time()
-
-    	if msg.tag == "right3":
-    		self.tag3_right_start = time.time()
-
-    	if msg.tag == "left4":
-    		self.tag4_left_start = time.time()
-
-    	if msg.tag == "right4":
-    		self.tag4_right_start = time.time()
-
-    #count the cost of each node (idleness)
-    def count_cost(self):
-    	self.tag1_left_cost = count_time(self.tag1_left_start)
-    	self.tag1_right_cost = count_time(self.tag1_right_start)
-    	self.tag2_left_cost = count_time(self.tag2_left_start)
-    	self.tag2_right_cost = count_time(self.tag2_left_start)
-    	self.tag3_left_cost = count_time(self.tag4_left_start)
-    	self.tag3_right_cost = count_time(self.tag3_right_start)
-    	self.tag4_left_cost = count_time(self.tag4_left_start)
-    	self.tag4_right_cost = count_time(self.tag4_right_start)
-
-    #initial time of all the nodes
     def start_time(self):
-		if not self.start: # if timer not start yet
-			self.timer_start = time.time() # record start time
-			self.tag1_left_start = self.timer_start
-			self.tag1_right_start = self.timer_start
-			self.tag2_left_start = self.timer_start
-			self.tag2_right_start = self.timer_start
-			self.tag3_left_start = self.timer_start
-			self.tag3_right_start = self.timer_start
-			self.tag4_left_start = self.timer_start
-			self.tag4_right_start = self.timer_start
+        if not self.start: # if timer not start yet
+            self.timer_start = time.time() # record start time
+            self.tag1_left_start = self.timer_start
+            self.tag1_right_start = self.timer_start
+            self.tag2_left_start = self.timer_start
+            self.tag2_right_start = self.timer_start
+            self.tag3_left_start = self.timer_start
+            self.tag3_right_start = self.timer_start
+            self.tag4_left_start = self.timer_start
+            self.tag4_right_start = self.timer_start
             self.start = True # change timer state to start
 
-    #return current time - starting time
     def count_time(self, t):
-		return time.time()-t
+        return time.time()-t
 
-    
+    def tag1_left(self, msg):
+        if msg.data == True:
+            self.tag1_left_cost.data = count_time(self.tag1_left_start)
+            self.pub_cost()
+            self.tag1_left_start = time.time()
+
+    def tag1_right(self, msg):
+        if msg.data == True:
+            self.tag1_right_cost.data = count_time(self.tag1_right_start)
+            self.pub_cost()
+            self.tag1_right_start = time.time()
+
+    def tag2_left(self, msg):
+        if msg.data == True:
+            self.tag2_left_cost.data = count_time(self.tag2_left_start)
+            self.pub_cost()
+            self.tag2_left_start = time.time()
+
+    def tag2_right(self, msg):
+        if msg.data == True:
+            self.tag2_right_cost.data = count_time(self.tag2_left_start)
+            self.pub_cost()
+            self.tag2_right_start = time.time()
+
+    def tag3_left(self, msg):
+        if msg.data == True:
+            self.tag3_left_cost.data = count_time(self.tag4_left_start)
+            self.pub_cost()
+            self.tag3_left_start = time.time()
+
+
+    def tag3_right(self, msg):
+        if msg.data == True:
+            self.tag3_right_cost.data = count_time(self.tag3_right_start)
+            self.pub_cost()
+            self.tag3_right_start = time.time()
+
+    def tag4_left(self, msg):
+        if msg.data == True:
+            self.tag4_left_cost.data = count_time(self.tag4_left_start)
+            self.pub_cost()
+            self.tag4_left_start = time.time()
+
+    def tag4_right(self, msg):
+        if msg.data == True:
+            self.tag4_right_cost.data = count_time(self.tag4_right_start)
+            self.pub_cost()
+            self.tag4_right_start = time.time()
 
 if __name__ == '__main__': 
     rospy.init_node('PatrollingNode',anonymous=False)
