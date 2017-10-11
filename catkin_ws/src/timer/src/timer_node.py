bc6c099a22dab8b7d04cccd159ae4f70c9444db3
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-10-11 14:17:24 +0800

    2017/10/11 patrolling

--- catkin_ws/src/timer/src/timer_node.py
+++ catkin_ws/src/timer/src/timer_node.py
@@ -24,7 +24,7 @@ class Timer(object):
 		self.sub_switch = rospy.Subscriber("~switch", BoolStamped, self.cbSwitch, queue_size=1)
 		self.sub_time_for_stop = rospy.Subscriber("~time_for_stop", Float32, self.cbtime, queue_size=1)
 		self.sub_mode = rospy.Subscriber("~command", Int8, self.command)
-		self.pub_command = rospy.Publisher("~command", Int8, queue_size=1)
+		#self.pub_command = rospy.Publisher("~command", Int8, queue_size=1)
 
 		self.pub_time_is_up = rospy.Publisher("~time_is_up", BoolStamped, queue_size=1, latch=True)
 		self.pub_forward = rospy.Publisher("~forward", BoolStamped, queue_size=1, latch=True)
