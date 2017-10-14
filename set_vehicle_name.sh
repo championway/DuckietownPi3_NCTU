8b0fa37c93622a6a2edabe98d28fbe2801442f48
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-10-14 18:40:41 +0800

    2017/10/14 make patroll adjustable

--- set_vehicle_name.sh
+++ set_vehicle_name.sh
@@ -3,8 +3,10 @@ echo "Setting VEHICLE_NAME..."
 if [ $# -gt 0 ]; then
 	# provided a hostname, use it as ROS_MASTER_URI
 	export VEHICLE_NAME=$1
+	export PATROLLING_NUMBER=$1
 else
 	echo "No hostname provided. Using $HOSTNAME."
 	export VEHICLE_NAME=$HOSTNAME
+	export PATROLLING_NUMBER=4
 fi
 echo "VEHICLE_NAME set to $VEHICLE_NAME"
