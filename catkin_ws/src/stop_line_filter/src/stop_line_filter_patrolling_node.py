208091cdc5c8de606195e4a764a19688db37beb7
championway <cpwearth.eed03@g2.nctu.edu.tw>
2017-10-22 15:35:26 +0800

    line_detector for demo

--- catkin_ws/src/stop_line_filter/src/stop_line_filter_patrolling_node.py
+++ catkin_ws/src/stop_line_filter/src/stop_line_filter_patrolling_node.py
@@ -75,7 +75,7 @@ class StopLineFilterNode(object):
             if segment.color != segment.RED:
                 return
             if segment.points[0].x < 0 or segment.points[1].x < 0: # the point is behind us 
-                return
+                continue
 
             p1_lane = self.to_lane_frame(segment.points[0])
             p2_lane = self.to_lane_frame(segment.points[1])
@@ -84,8 +84,7 @@ class StopLineFilterNode(object):
             stop_line_x_accumulator += avg_x
             stop_line_y_accumulator += avg_y # TODO output covariance and not just mean
             good_seg_count += 1.0
-            #print "-----", good_seg_count, "-----"
-        print "here A"
+
         stop_line_reading_msg = StopLineReading()
         stop_line_reading_msg.header.stamp = segment_list_msg.header.stamp
         if (good_seg_count < self.min_segs):
@@ -93,7 +92,7 @@ class StopLineFilterNode(object):
             stop_line_reading_msg.at_stop_line = False
             self.pub_stop_line_reading.publish(stop_line_reading_msg)
             return
-        print "here B"
+        
         stop_line_reading_msg.stop_line_detected = True
         stop_line_point = Point()
         stop_line_point.x = stop_line_x_accumulator/good_seg_count
@@ -101,16 +100,13 @@ class StopLineFilterNode(object):
         stop_line_reading_msg.stop_line_point = stop_line_point
         #stop_line_reading_msg.at_stop_line = stop_line_point.x < self.stop_distance and math.fabs(stop_line_point.y) < self.lanewidth/4 
         stop_line_reading_msg.at_stop_line = stop_line_point.x < self.stop_distance
-        #stop_line_reading_msg.at_stop_line = True
-        self.pub_stop_line_reading.publish(stop_line_reading_msg)  
-        print "here C"  
+        self.pub_stop_line_reading.publish(stop_line_reading_msg)    
         if stop_line_reading_msg.at_stop_line:
             msg = BoolStamped()
             msg.header.stamp = stop_line_reading_msg.header.stamp
             msg.data = True
-            print"here D"
+            
             if segment.color == segment.RED:
-                print "detect stop line"
                 if self.past_patrol_info != self.patrol_info:
                     self.past_patrol_info = self.patrol_info
                     print "-------at stop line-------"
